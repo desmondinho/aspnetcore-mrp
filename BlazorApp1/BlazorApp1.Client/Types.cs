8c6a09d84521c438822e2c24fe53ee042df30701
desmondinho <daniil.norinn@gmail.com>
2024-09-14 21:02:31 +0300

    Add project files.

---
+++ BlazorApp1/BlazorApp1.Client/Types.cs
@@ -0,0 +1,41 @@
+﻿namespace BlazorApp1.Client;
+
+public class Ok( string value, string value2 )
+{
+    public string Value { get; } = value;
+    public string Value2 { get; } = value2;
+}
+
+public class NotOk
+{
+    public string Value { get; }
+    public string? Value2 { get; }
+
+    public NotOk( string value )
+    {
+        Value = value;
+    }
+
+    public NotOk( string value, string value2 ) : this( value )
+    {
+        Value2 = value2;
+    }
+}
+
+public class AnotherNotOk
+{
+    public string? Value { get; }
+    public string? Value2 { get; }
+
+    public AnotherNotOk() { }
+
+    public AnotherNotOk( string value )
+    {
+        Value = value;
+    }
+
+    public AnotherNotOk( string value, string value2 ) : this( value )
+    {
+        Value2 = value2;
+    }
+}
